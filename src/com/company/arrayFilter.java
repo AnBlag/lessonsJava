31841a1fa7df2c54b1283273862102544e311a9c
AnBlag <andrey.blagin@yandex.ru>
2021-02-26 20:37:43 +0300

    Добавлен класс сортировки массива и фильтрации повторяющихся чисел.

---
+++ src/com/company/arrayFilter.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class arrayFilter {
+    public static void main(String[] args) {
+
+        int[] array = {10, 2, 10, 3, 1, 2, 5};
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+
+            int min = array[i];
+            int min_i = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                int tmp = array[i];
+                array[i] = array[min_i];
+                array[min_i] = tmp;
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            int temp = array[i];
+            if (i == 0){
+                list.add(temp);
+            }
+            else if (temp == array[i-1]){
+
+            }
+            else list.add(array[i]);
+        }
+        System.out.println(list);
+    }
+}
