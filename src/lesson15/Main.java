54500c07ac86a1f75b8688bd0c1515ae83d14539
AnBlag <andrey.blagin@yandex.ru>
2021-03-24 21:42:46 +0300

    Добавлен ДЗ 15го урока.

---
+++ src/lesson15/Main.java
@@ -0,0 +1,30 @@
+package lesson15;
+
+public class Main {
+    public static void main(String[] args) {
+
+       Thread thread = new Threads();
+
+        process(thread);
+
+
+
+    }
+
+        public static void process(Thread thread) {
+            switch (thread.getState()) {
+                case NEW:
+                    thread.start();
+                    break;
+                case WAITING:
+                    thread.interrupt();
+                    break;
+                case RUNNABLE:
+                    thread.isInterrupted();
+                    break;
+                case TERMINATED:
+                    System.out.println(thread.getPriority());
+                    break;
+            }
+        }
+}
---
+++ src/lesson15/Threads.java
@@ -0,0 +1,16 @@
+package lesson15;
+
+public class Threads extends Thread{
+
+
+    public Threads() {
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+
+}
